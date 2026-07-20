81760820b3abef38ed638a179c054f9e5e6ee548
VisionCrizzal <nguyengocvi94tn@gmail.com>
2026-07-20 09:44:47 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace blogmanager_nguyenngocvi_22t1020794.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
